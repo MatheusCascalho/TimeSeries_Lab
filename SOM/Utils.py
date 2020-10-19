151ecb17d65fa40b4f2b38a88d146cb569841ca1
Matheus Cascalho <matheus.santos@enacom.com.br>
2020-10-19 18:59:58 -0300

    conversão de imagens em vetores 3d

--- SOM/Utils.py
+++ SOM/Utils.py
@@ -18,19 +18,32 @@ def rename_images(path: str, to_dir: str = None, extension: str = "png") -> None
         min = file[10:12]
         name = f'{year}_{month}_{day} {hour}:{min}.{extension}'
         if to_dir is not None:
-            copyfile(path + '/' +file,
+            copyfile(path + '/' + file,
                      to_dir + '/' + name)
         else:
             os.rename(path + '/' + file,
                       path + '/' + name)
 
 
-def dir_to_data(path: str, extension: str = "png", qtt: int = 10):
+def dir_to_data(path: str,
+                extension: str = 'jpg',
+                qtt: int = 10,
+                convert_type: str = 'RGB') -> list:
     files = [img for img in os.listdir(path) if img.endswith(extension)]
     if len(files) >= qtt:
         files = files[:qtt]
+    data_images = []
+    for file in files:
+        filepath = path + '/' + file
+        img = Image.open(filepath)
+        data = np.asarray(img.convert(convert_type))
+        data_images.append(data)
+    return data_images
+
 
 if __name__ == '__main__':
     path = 'images/20201019'
     dst = 'images/dataset'
-    rename_images(path, to_dir=dst, extension='raw.jpg')
+    # rename_images(path, to_dir=dst, extension='raw.jpg')
+    dataset = dir_to_data(dst, qtt=3)
+    print(dataset[2])
